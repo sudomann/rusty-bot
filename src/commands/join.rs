bee2bb1f53be49c4e22e4b1dea3b01f4784fd9b2
Willy Njundong <njundongw@gmail.com>
2021-04-22 01:48:32 -0400

    fix: join command correctly updates state
    - vacate players from pug join collection
    -now stores game modes they were removed from 
    - correctly creates a new picking session

--- src/commands/join.rs
+++ src/commands/join.rs
@@ -1,8 +1,9 @@
 use crate::{
-    pug::{GameMode, Participants, Player},
+    pug::{GameMode, PickingSession, Player},
     validation::{game_mode::*, multiple_fill::*},
-    PugsWaitingToFill, RegisteredGameModes,
+    FilledPug, PugsWaitingToFill, RegisteredGameModes,
 };
+
 use linked_hash_set::LinkedHashSet;
 use serenity::{
     framework::standard::{macros::command, Args, CommandResult},
@@ -10,7 +11,7 @@ use serenity::{
     prelude::*,
     utils::MessageBuilder,
 };
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[command]
 #[aliases("j", "jp")]
@@ -35,50 +36,117 @@ async fn join(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
                 .clone()
         };
 
-        let registered_game_modes = lock_for_registered_game_modes.read().await;
+        let lock_for_filled_pugs = {
+            let data_write = ctx.data.read().await;
+            data_write
+                .get::<FilledPug>()
+                .expect("Expected FilledPug in TypeMap")
+                .clone()
+        };
+
+        let registered_game_modes_in_guild = lock_for_registered_game_modes.read().await;
         // TODO: what if PugsWaitingToFill/RegisteredGameModes not available for a a particular guild?
         // i.e. `[registered_game_modes | pugs_waiting_to_fill ].get(&guild_id)` is `None`
 
-        if let Some(_game_modes) = registered_game_modes.get(&guild_id) {
+        if let Some(registered_game_modes) = registered_game_modes_in_guild.get(&guild_id) {
             let mut pugs_waiting_to_fill = lock_for_pugs_waiting_to_fill.write().await;
 
             if let Some(pugs_waiting_to_fill_in_guild) = pugs_waiting_to_fill.get_mut(&guild_id) {
                 let desired_game_mode_args = args
                     .iter::<String>()
-                    .filter_map(|arg| arg.ok())
+                    .filter(|arg| arg.is_ok())
+                    .map(|arg| arg.unwrap().to_lowercase())
                     .collect::<HashSet<String>>();
 
-                for (game_mode, participants) in pugs_waiting_to_fill_in_guild.iter_mut() {
-                    if desired_game_mode_args.contains(game_mode.key()) {
-                        // add user to pug
-                        let player = Player::new(msg.author.id);
-                        // multiple join attempts wont result in duplicate
-                        // instances of PugParticipant in LinkedHashSet
-                        let mut just_filled = false;
-                        if participants.is_empty() {
-                            participants.insert(player);
-                        } else {
-                            participants.insert(player);
-                            just_filled = participants.len() as u8 == game_mode.capacity();
-                        }
-                        if just_filled {
+                let mut game_mode_about_to_fill: Option<&GameMode> = None;
+                let matches_some_desired_game_mode =
+                    |arg: &&GameMode| desired_game_mode_args.contains(arg.key());
+                let owned = registered_game_modes.clone();
+                let game_modes_to_join = owned.iter().filter(matches_some_desired_game_mode);
 
+                for game_mode in game_modes_to_join {
+                    if let Some(participants) = pugs_waiting_to_fill_in_guild.get(game_mode) {
+                        // check occupancy of game modes the user is joining
+                        // and store the first one that is going to fill
+                        if game_mode.capacity() == (participants.len() as u8 + 1) {
+                            game_mode_about_to_fill = Some(game_mode);
+                            // stop checking because user can only fill one pug at a time
+                            break;
+                        }
+                    }
+                }
+
+                match game_mode_about_to_fill {
+                    Some(filled_game_mode) => {
+                        let mut player_copy: Option<LinkedHashSet<Player>> = None;
+                        // get filled game mode participants
+                        if let Some(filled_pug_players) =
+                            pugs_waiting_to_fill_in_guild.get(filled_game_mode)
+                        {
                             // announce pug has filled
-
-                            // remove it from vec
-
-                            // remove players in this pug from any other pugs they're in
-                            // and announce
-
-                            // create PickingSession, discarding PugWaitingToFill
-
-                            // in picking session, there should be a reference to the announcement,
-                            // which updates every second with auto captain countdown
+                            msg.channel_id
+                                .say(
+                                    &ctx.http,
+                                    MessageBuilder::new()
+                                        .push(format!("{:?} has filled!", filled_game_mode))
+                                        .push_bold_line(format!(
+                                            "TODO - ping & DM participants: {:?}",
+                                            filled_pug_players
+                                        )),
+                                )
+                                .await?;
+                            player_copy = Some(filled_pug_players.clone());
                         }
+
+                        // TODO: Notify all removed players
+                        let mut _removals: HashMap<&GameMode, &UserId> = HashMap::default();
+
+                        // then loop through all game modes:
+                        // - players that are in the filled pug are removed from all other game modes
+                        // - if currently evaluating the filled game mode, move all players to a PickingSession
+                        for (current_game_mode, participants) in
+                            pugs_waiting_to_fill_in_guild.iter_mut()
+                        {
+                            if current_game_mode == filled_game_mode {
+                                let mut filled_pugs = lock_for_filled_pugs.write().await;
+
+                                if let Some(filled_pugs_in_guild) = filled_pugs.get_mut(&guild_id) {
+                                    let picking_session = PickingSession::new(
+                                        &current_game_mode,
+                                        participants.clone(),
+                                    );
+                                    filled_pugs_in_guild.push_back(picking_session);
+                                }
+
+                                // clear players from this pug
+                                participants.clear();
+
+                                // in picking session, there should be a reference to the announcement,
+                                // which updates every second with auto captain countdown
+                            } else {
+                                // remove them from *other* pugs they're in
+                                if let Some(ref filled_pug_players) = player_copy {
+                                    for player in filled_pug_players {
+                                        if participants.remove(player) {
+                                            _removals
+                                                .insert(current_game_mode, player.get_user_id());
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    None => {
+                        // insert user to pugs they want to join
+                        for (_game_mode, participants) in pugs_waiting_to_fill_in_guild.iter_mut() {
+                            // check if one of desired game modes before joining
+                            participants.insert(Player::new(msg.author.id));
+                        }
+
                         msg.reply(
                             &ctx.http,
                             MessageBuilder::new()
-                                .push(format!("{:?}", participants))
+                                .push(format!("TODO - composition of joined pugs"))
                                 .build(),
                         )
                         .await?;
