12c11f20e200db61d838ca3b4a9fdd01c82137d9
Willy Njundong <njundongw@gmail.com>
2021-04-22 02:00:43 -0400

    fix bad return of "no games registered" message
    `let game_modes = ...` now used directly as an iterator.
    Old implementation unnecessarily collected elements into a hashset from which another iterator was obtained. :facepalm:

--- src/validation/multiple_fill.rs
+++ src/validation/multiple_fill.rs
@@ -1,4 +1,4 @@
-use crate::{pug::GameMode, PugsWaitingToFill, RegisteredGameModes};
+use crate::{PugsWaitingToFill, RegisteredGameModes};
 use serenity::{
     framework::standard::{macros::check, Args, CommandOptions, Reason},
     model::prelude::*,
@@ -29,7 +29,6 @@ pub async fn is_filling_more_than_one_pug_check(
                 .clone();
             let registered_game_modes = lock_for_registered_game_modes.read().await;
 
-            let mut game_modes: HashSet<&GameMode> = HashSet::new();
             if let Some(game_modes_in_guild) = registered_game_modes.get(&guild_id) {
                 let game_modes_to_join = args
                     .clone()
@@ -39,53 +38,57 @@ pub async fn is_filling_more_than_one_pug_check(
                     // because it will be compared to lowercased game label (GameMode.key)
                     .map(|arg| arg.unwrap().to_lowercase())
                     .collect::<HashSet<String>>();
-                game_modes = game_modes_in_guild
+                let game_modes = game_modes_in_guild
                     .iter()
-                    .filter(|game_mode| game_modes_to_join.contains(game_mode.key()))
-                    .collect();
-            };
+                    .filter(|game_mode| game_modes_to_join.contains(game_mode.key()));
 
-            let lock_for_pugs_waiting_to_fill = data_read
-                .get::<PugsWaitingToFill>()
-                .expect("Expected PugsWaitingToFill in TypeMap")
-                .clone();
-            let pugs_waiting_to_fill = lock_for_pugs_waiting_to_fill.read().await;
+                let lock_for_pugs_waiting_to_fill = data_read
+                    .get::<PugsWaitingToFill>()
+                    .expect("Expected PugsWaitingToFill in TypeMap")
+                    .clone();
+                let pugs_waiting_to_fill = lock_for_pugs_waiting_to_fill.read().await;
 
-            match pugs_waiting_to_fill.get(&guild_id) {
-                Some(potential_pugs) => {
-                    let mut game_modes_which_will_fill = Vec::default();
-                    for game_mode in game_modes.iter() {
-                        if let Some(participants) = potential_pugs.get(&game_mode) {
-                            if participants.is_empty() {
-                                continue;
-                            } else {
-                                let will_fill_on_join =
-                                    game_mode.capacity() - participants.len() as u8 == 1;
-                                if will_fill_on_join {
-                                    game_modes_which_will_fill.push(game_mode);
+                match pugs_waiting_to_fill.get(&guild_id) {
+                    Some(potential_pugs) => {
+                        let mut game_modes_which_will_fill = Vec::default();
+                        for game_mode in game_modes {
+                            if let Some(participants) = potential_pugs.get(game_mode) {
+                                if participants.is_empty() {
+                                    continue;
+                                } else {
+                                    let will_fill_on_join =
+                                        game_mode.capacity() - participants.len() as u8 == 1;
+                                    if will_fill_on_join {
+                                        game_modes_which_will_fill.push(game_mode);
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    if game_modes_which_will_fill.len() > 1 {
-                        Some(
-              MessageBuilder::new()
-                .push("Ignored\n")
-                .push("You may not fill more than one pug at a time\n")
-                .push("More than one of the game modes you tried to join will fill:\n")
-                .push(format!("{:?}", game_modes_which_will_fill))
-                .build(),
-            )
-                    } else {
-                        None
+                        if game_modes_which_will_fill.len() > 1 {
+                            Some(
+                  MessageBuilder::new()
+                    .push("Ignored\n")
+                    .push("You may not fill more than one pug at a time\n")
+                    .push("More than one of the game modes you tried to join will fill:\n")
+                    .push(format!("{:?}", game_modes_which_will_fill))
+                    .build(),
+                )
+                        } else {
+                            None
+                        }
+                    }
+                    None => {
+                        Some("Cannot join this game mode because a matching key wasn't found in hash map"
+                            .to_string())
                     }
                 }
-                None => Some(
+            } else {
+                Some(
                     MessageBuilder::new()
                         .push("No game modes registered. Contact admins to run `.addmod`")
                         .build(),
-                ),
+                )
             }
         };
         if let Some(response) = fail {
