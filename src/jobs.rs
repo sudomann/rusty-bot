d364d3ad62685e9d2ddd3ce221541dbb05d54caa
Willy Njundong <njundongw@gmail.com>
2021-05-29 00:20:26 -0400

    add notes

--- src/jobs.rs
+++ src/jobs.rs
@@ -29,3 +29,56 @@ TODO:
 when db backup job runs, makes sure all the various collections of in-memory
 storages stay around a given length. i.e. after backing up, trim as neccessary, discarding the oldest entries
 */
+
+/*
+
+    Anything with PickingSession needs to be considerately stored
+    and retrieved
+
+
+
+    DesignatedPugChannel:
+    -   on create, add to db first, then memory
+    -   on delete, remove from db first, then remove from mem
+
+    RegisteredGameModes:
+    -   on create, add to db first, then memory
+    -   on delete, remove from db first, then remove from mem
+
+    PugsWaitingToFill:
+    -   on join, add to memory
+    -   on leave, remove from memory
+
+    FilledPug, CompletedPug:
+    -   .reset alters mem only
+    -   .pick alters mem only
+    -   .leave alters mem only
+    -   .quit command, SIGTERM, SIGINT attempt to add to db before shutdown
+
+
+    DefaultVoiceChannels:
+    -   on create, add to db first, then memory
+    -   on delete, remove from db first, then remove from mem
+    -   on cache_ready(), load all from db into memory
+
+    * On cache_ready, load all from db into memory:
+    -   DesignatedPugChannel
+    -   RegisteredGameModes
+    -   PugsWaitingToFill
+
+
+    * DB storage job every 1 min:
+    -   PugsWaitingToFill
+    -   FilledPug
+    -   CompletedPug
+    -   Only store if item type has more than 5 (save as const) elements currently in memory
+
+    * DB storage on .quit command, SIGTERM, SIGINT signals:
+    -   PugsWaitingToFill
+    -   FilledPug
+    -   CompletedPug
+
+
+    * Every 5 mins, check for players that have been in pug for over 5 hours
+
+*/
