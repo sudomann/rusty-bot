3e54b5033c3149b4e7e761c9f8b600cb5fa0bce7
Willy Njundong <njundongw@gmail.com>
2021-05-29 00:21:37 -0400

    add check for presence status

--- src/checks.rs
+++ src/checks.rs
@@ -1,2 +1,3 @@
 pub mod guild_privilege;
+pub mod presence_status;
 pub mod pug_channel;
---
+++ src/checks/presence_status.rs
@@ -0,0 +1,43 @@
+use serenity::{
+    framework::standard::{macros::check, Args, CommandOptions, Reason},
+    model::prelude::{OnlineStatus::*, *},
+    prelude::*,
+};
+
+const NO_INVIS_OR_OFFLINE_MSG: &str = "Invisible/Offline players cannot participate in pugs";
+
+#[check]
+#[name = "NoInvisbleOrOfflineStatus"]
+async fn is_not_invisible_or_offline_status(
+    ctx: &Context,
+    msg: &Message,
+    _: &mut Args,
+    _: &CommandOptions,
+) -> Result<(), Reason> {
+    let guild_id = msg.guild_id.unwrap();
+
+    if let Some(guild) = guild_id.to_guild_cached(&ctx).await {
+        // Note: with serenity 0.10.7, `guild.presences` seems to contain
+        // presence data only for the users current status is Online/Idle/Do Not Disturb
+        // This does not line up with what the 0.10.7 docs may lead you to believe:
+        // if you have the GatewayIntents::GUILD_MEMBERS intent, then `guild.presences`
+        // is populated - if you don't then it's empty
+        //
+        // SO, we proceed by using guild.presences.get(&msg.author.id) ---> None to conclude
+        // that the user is offline, since we've enabled using the GatewayIntents::GUILD_MEMBERS intent
+        let presence = guild.presences.get(&msg.author.id);
+        if presence.is_none() {
+            return Err(Reason::User(NO_INVIS_OR_OFFLINE_MSG.to_string()));
+        }
+
+        // Despite the behavior described above, we will still check the unwrapped presence status
+        // in case there is inconsistency with the population of `guild.presences` data
+        match presence.unwrap().status {
+            Invisible | Offline => {
+                return Err(Reason::User(NO_INVIS_OR_OFFLINE_MSG.to_string()));
+            }
+            _ => {}
+        }
+    }
+    Ok(())
+}
