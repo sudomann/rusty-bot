2ae6abdbe7e0b88b4aa5127d831333adba18013b
Willy Njundong <njundongw@gmail.com>
2021-07-05 17:15:08 -0400

    Add check GuildDataSyncInProgress

--- src/checks.rs
+++ src/checks.rs
@@ -1,3 +1,4 @@
 pub mod guild_privilege;
 pub mod presence_status;
 pub mod pug_channel;
+pub mod sync_in_progress;
---
+++ src/checks/sync_in_progress.rs
@@ -0,0 +1,28 @@
+use serenity::{
+    framework::standard::{macros::check, Args, CommandOptions, Reason},
+    model::channel::Message,
+    prelude::*,
+};
+
+#[check]
+#[name = "GuildDataSyncInProgress"]
+#[check_in_help(false)]
+#[display_in_help(false)]
+async fn is_guild_data_sync_in_progress(
+    _: &Context,
+    msg: &Message,
+    _: &mut Args,
+    _: &CommandOptions,
+) -> Result<(), Reason> {
+    // TODO: Check check memory to see if message guild is
+    // currently being synced.
+    Err(Reason::UserAndLog {
+        user: "Please wait a little longer to use this command.\
+        \nThis this server's data is currently being synced to a database."
+            .to_string(),
+        log: format!(
+            "Command by {} - {} was was ignored because their guild's data is being synced: {:?}",
+            msg.author.id, msg.author.name, msg.guild_id
+        ),
+    })
+}
