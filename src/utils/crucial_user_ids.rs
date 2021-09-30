d5dcdafaffaf5339313e63726d840c8bfc648219
Willy Njundong <njundongw@gmail.com>
2021-09-30 18:28:26 -0400

    better handling of superuser ids

--- src/utils/crucial_user_ids.rs
+++ src/utils/crucial_user_ids.rs
@@ -1,5 +1,6 @@
 use serenity::{http::Http, model::id::UserId, Error};
 use std::{collections::HashSet, env, str::FromStr};
+use tracing::{info, instrument, warn};
 
 pub struct CrucialUserIds {
     bot: UserId,
@@ -18,25 +19,26 @@ impl CrucialUserIds {
 
 /// Get the Bot/Application's [`UserId`] along with those provided to the environment
 /// variable `SUPERUSERS` (provided they are correctly formatted - if not, they are ignored).
+
+#[instrument]
 pub async fn obtain(http: Http) -> Result<CrucialUserIds, Error> {
     match http.get_current_application_info().await {
         Ok(info) => {
             let mut superusers: HashSet<UserId> = match env::var("SUPERUSERS") {
-                Ok(superusers) => {
-                    let superuser_ids: HashSet<&str> = superusers.split_terminator(',').collect();
-                    superuser_ids
-                        .iter()
-                        .filter_map(|id| UserId::from_str(id).ok())
-                        .collect()
-                }
+                Ok(superusers) => superusers
+                    .split_terminator(',')
+                    .filter_map(|id| UserId::from_str(id).ok())
+                    .collect(),
                 Err(_err) => {
-                    // TODO: announce that there was an error parsing the provided superusers list,
-                    // so there will be no superusers aside from the bot/application owner
+                    warn!(
+                        "SUPERUSERS was not found in the environment, \
+                        so there will be no superusers aside from the owner bot/application"
+                    );
                     HashSet::default()
                 }
             };
             superusers.insert(info.owner.id);
-
+            info!("Superusers: {:?}", superusers);
             Ok(CrucialUserIds {
                 bot: info.id,
                 superusers,
