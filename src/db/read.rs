23ceac0703c9eaaa0f1b5370eb3aaa65b23550d4
Willy Njundong <njundongw@gmail.com>
2021-10-01 09:49:16 -0400

    add db::read function to fetch saved guilds

--- src/db/read.rs
+++ src/db/read.rs
@@ -1,1 +1,10 @@
 use super::model::*;
+use futures::stream::TryStreamExt;
+use mongodb::{error::Error, Database};
+
+pub async fn get_registered_guilds(db: Database) -> Result<Vec<Guild>, Error> {
+    let collection = db.collection::<Guild>("guilds");
+    let cursor = collection.find(None, None).await?;
+    let v: Vec<Guild> = cursor.try_collect().await?;
+    Ok(v)
+}
