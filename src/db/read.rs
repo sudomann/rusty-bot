165147ca685d962b1b7c96615da5285c343216c1
Willy Njundong <njundongw@gmail.com>
2021-10-02 21:51:38 -0400

    add helpers to retrieve guild data from db

--- src/db/read.rs
+++ src/db/read.rs
@@ -1,12 +1,23 @@
 use futures::stream::TryStreamExt;
+use mongodb::bson::doc;
 use mongodb::error::Error;
 use mongodb::Database;
+use serenity::model::id::GuildId;
 
 use super::model::*;
 
-pub async fn get_registered_guilds(db: Database) -> Result<Vec<Guild>, Error> {
+/// Get list of known guilds
+pub async fn get_known_guilds(db: Database) -> Result<Vec<Guild>, Error> {
     let collection = db.collection::<Guild>("guilds");
     let cursor = collection.find(None, None).await?;
-    let v: Vec<Guild> = cursor.try_collect().await?;
-    Ok(v)
+    Ok(cursor.try_collect().await?)
+}
+
+/// Fetch a known guild by it's guild id
+pub async fn get_guild(db: Database, guild_id: GuildId) -> Result<Option<Guild>, Error> {
+    let collection = db.collection::<Guild>("guilds");
+    let filter = doc! {
+        "guild_id": *guild_id.as_u64() as i64,
+    };
+    Ok(collection.find_one(Some(filter), None).await?)
 }
