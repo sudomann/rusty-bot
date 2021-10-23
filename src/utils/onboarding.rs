de65c13a867e3d3ceb1f545078585b71a1a82998
Willy Njundong <njundongw@gmail.com>
2021-10-23 17:42:22 -0400

    onboarding polishes
    - now also clears saved commands in db after deleting all guild commands

--- src/utils/onboarding.rs
+++ src/utils/onboarding.rs
@@ -7,9 +7,10 @@ use serenity::client::Context;
 use serenity::model::id::GuildId;
 use tokio::task::JoinHandle;
 use tokio::time::{interval, Duration};
-use tracing::{error, info, instrument};
+use tracing::{error, info, instrument, warn};
 
 use crate::db::model::GuildCommand;
+use crate::db::write::clear_guild_commands;
 use crate::DbClientRef;
 
 /// For each guild, check for presence of guild application commands created by this bot.
@@ -44,17 +45,8 @@ pub async fn inspect_guild_commands(ctx: Arc<Context>, guild_ids: Vec<GuildId>) 
     for handle in join_all(join_handles).await {
         match handle {
             Ok(result) => {
-                match result {
-                    Ok(_g) => {
-                        // Considering that printing to stdout on every iteration is slow
-                        // Is it worth announcing anything for this?
-                    }
-                    Err(err) => {
-                        error!(
-                            "Error during an attempt to write to the database:\n {:?}",
-                            err
-                        );
-                    }
+                if let Err(err) = result {
+                    error!("{:?}", err);
                 }
             }
             Err(join_err) => {
@@ -73,19 +65,16 @@ async fn inspect_and_maybe_update_db(
     guild_id: GuildId,
     db_client: Client,
 ) -> Result<GuildId, crate::error::Error> {
-    let commands = guild_id.get_application_commands(&ctx.http).await?;
-
-    let db = db_client.database("guild_commands");
+    let db = db_client.database(guild_id.to_string().as_str());
 
     // get commands saved in db
     let mut cursor = db
-        .collection(guild_id.to_string().as_str())
+        .collection::<GuildCommand>("guild_commands")
         .find(None, None)
         .await?;
-    let mut saved_commands: Vec<GuildCommand> = Vec::default();
 
     let current_commands = guild_id.get_application_commands(&ctx.http).await?;
-
+    let mut saved_commands: Vec<GuildCommand> = Vec::default();
     while let Some(saved_command) = cursor.try_next().await? {
         saved_commands.push(saved_command);
     }
@@ -97,11 +86,17 @@ async fn inspect_and_maybe_update_db(
             .iter()
             .all(|current| saved_commands.iter().any(|saved| saved.eq(current)));
     if !commands_match {
+        warn!(
+            "Guild command mismatch for {}. Clearing all existing from guild and database...",
+            &guild_id
+        );
         // clear guild commands
         guild_id.set_application_commands(&ctx.http, |c| c).await?;
+        clear_guild_commands(db.clone()).await?;
     }
 
     if saved_commands.is_empty() {
+        info!("Creating /setup command for {}", &guild_id);
         // create /setup command
         let setup_cmd = guild_id
             .create_application_command(&ctx.http, |c| {
