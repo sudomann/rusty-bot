7ce658fe10e97bc5994fcad43d5d0484f3786aec
Willy Njundong <njundongw@gmail.com>
2021-10-23 17:39:47 -0400

    add GuildCommand model

--- src/db/model.rs
+++ src/db/model.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serenity::model::interactions::application_command::ApplicationCommand;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Guild {
@@ -26,4 +27,18 @@ pub struct GameMode {
 pub struct Player {
     pub user_id: u64,
     pub join_datetime: DateTime<Utc>,
+}
+
+/// Basically a slimmed down [`serenity::model::interactions::application_command::ApplicationCommand`]
+/// with only the field we need to check/store in the database.
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+pub struct GuildCommand {
+    pub command_id: u64,
+    pub name: String,
+}
+
+impl PartialEq<ApplicationCommand> for GuildCommand {
+    fn eq(&self, other: &ApplicationCommand) -> bool {
+        other.id == self.command_id
+    }
 }
