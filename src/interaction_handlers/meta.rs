a8ff138c71c04473181b704ad5e6e435dcd50511
Willy Njundong <njundongw@gmail.com>
2022-01-30 15:36:50 -0500

    add empty handler for browsing completed pug history

--- src/interaction_handlers/meta.rs
+++ src/interaction_handlers/meta.rs
@@ -1,3 +1,5 @@
+use serenity::client::Context;
+use serenity::model::interactions::application_command::ApplicationCommandInteraction;
 use serenity::utils::MessageBuilder;
 
 pub fn render_help_text() -> String {
@@ -6,4 +8,20 @@ pub fn render_help_text() -> String {
         .push_line("Unimplemented")
         .push_line("Server admins, use `.configure` to set up (or update) my slash commands.");
     response.to_string()
+}
+
+pub async fn pug_history(
+    _ctx: &Context,
+    _interaction: &ApplicationCommandInteraction,
+) -> anyhow::Result<String> {
+    // validate this channel is a GuildChannel
+    // with kind PublicThread
+
+    // check for an active pug
+
+    // Clear all captains and picks
+
+    // Restart autocap timer
+
+    Ok("Sorry, this feature is incomplete".to_string())
 }
